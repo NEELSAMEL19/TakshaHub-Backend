e477dffab5d75b35b793ea66fc0a6581d9ecb4b3
Neel Samel <162904525+NEELSAMEL19@users.noreply.github.com>
2026-06-17 11:13:18 +0530

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,12 +3,10 @@ import { asyncHandler } from "../../common/utils/utils.js";
 import { AuthService } from "./auth.service.js";
 
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const cookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: isProduction,
-  sameSite: isProduction ? "none" : "lax",
+  secure: true,
+  sameSite: "none" ,
   maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 };
